0faaf7943e2239e285641427237230b147c28fc3
mldelaro <mldelaro@asu.edu>
2015-02-10 18:23:22 -0700

    Update Roster object to provide statically generated stub content

--- BeaconServlet/src/com/ticketmaster/api/rest/Roster.java
+++ BeaconServlet/src/com/ticketmaster/api/rest/Roster.java
@@ -1,5 +1,6 @@
 package com.ticketmaster.api.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.DefaultValue;
@@ -9,9 +10,13 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.ticketmaster.bean.EventBean;
 import com.ticketmaster.bean.RosterEntryBean;
+import com.ticketmaster.bean.UserBean;
+import com.ticketmaster.dao.MySqlDaoFactory;
+import com.ticketmaster.dao.UserDao;
 
 @Path("rest/Roster")
 public class Roster {
@@ -23,12 +28,46 @@ public class Roster {
 	public List<EventBean> responseGetEventsWithRoster() {
 		return null;
 	}
-	/*
+	
 	// Event ID parameter -> Get roster for event
 	@GET
 	@Path("Event")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<RosterEntryBean> responseGetUsersAtRoster(@FormParam("eventId") @DefaultValue("-1") int eventId) {
+		List<RosterEntryBean> completeRoster = new ArrayList<RosterEntryBean>();
+		
+		UserBean usr1 = new UserBean(1, "John", "Doe", "Jdoe", "pass");
+		UserBean usr2 = new UserBean(2, "Jane", "Doe", "Jdoe", "pass");
+		UserBean usr3 = new UserBean(3, "LOL", "DOUGH", "Jdoe", "pass");
+		UserBean usr4 = new UserBean(4, "TheLastPerson", "ToEnter", "lastty", "pass");
+		
+		EventBean evt1 = new EventBean(1, "Event1");
+		EventBean evt2 = new EventBean(2, "Why are you here?");
+		EventBean evt3 = new EventBean(3, "Forever Alone");
+		
+		RosterEntryBean entry1 = new RosterEntryBean(evt1, usr1, true);
+		RosterEntryBean entry2 = new RosterEntryBean(evt1, usr2, true);
+		RosterEntryBean entry3 = new RosterEntryBean(evt1, usr3, true);
+		RosterEntryBean entry4 = new RosterEntryBean(evt1, usr4, true);
+		
+		RosterEntryBean entry5 = new RosterEntryBean(evt2, usr1, true);
+		RosterEntryBean entry6 = new RosterEntryBean(evt2, usr2, true);
+		
+		
+		if(eventId == 1) {
+			completeRoster.add(entry1);
+			completeRoster.add(entry2);
+			completeRoster.add(entry3);
+			completeRoster.add(entry4);
+			
+			return completeRoster;
+		} else if (eventId == 2) {
+			completeRoster.add(entry5);
+			completeRoster.add(entry6);
+			
+			return completeRoster;
+		}
+		
 		return null;
 	}
 	
@@ -36,9 +75,27 @@ public class Roster {
 	@POST
 	@Path("User")
 	@Produces(MediaType.APPLICATION_JSON)
-	public RosterEntryBean setUserAttend(@FormParam("eventId") @DefaultValue("-1") int eventId,
+	public Response setUserAttend(@FormParam("eventId") @DefaultValue("-1") int eventId,
 										 @FormParam("username") @DefaultValue("") String username) {
-		return null;
+		boolean didFindUser = false;
+		
+		try {
+	        UserDao userDao = MySqlDaoFactory.getUserDAO();
+	        List<UserBean> userList = userDao.getAllUsers();
+	        for(int i = 0; i < userList.size(); i++) {
+	        	if(userList.get(i).getUsername().compareTo(username) == 0) {
+	        		didFindUser = true;
+	        	}
+	        }
+        } catch (Exception Ex) {
+        	return Response.status(500).entity("Error getting user list").build();
+        }
+		
+		if(didFindUser && eventId < 10) {
+			String response = "welcome to event " + eventId;
+			return Response.status(200).entity(response).build();
+		}
+		
+		return Response.status(200).entity("Invalid user, or no ticket?").build();
 	}
-	*/
 }
