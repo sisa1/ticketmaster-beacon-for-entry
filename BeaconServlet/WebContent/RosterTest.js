989b4632ea62d6afc41e4f8a338d3f8371f305d9
Shelby <sisa1@asu.edu>
2015-02-11 09:21:27 -0700

    RosterTest: attempt 3

--- BeaconServlet/WebContent/RosterTest.js
+++ BeaconServlet/WebContent/RosterTest.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
 		$.each(data, function(i, item) {
 			$("#results").append(
 				"<p>Visitor ID: " + item.visitor.id + "</p>" +
+				"<p>Visitor Username: " + item.visitor.username + "</p>" +
+				"<p>Visitor Password: " + item.visitor.password + "</p>" +
 				"<p>First name: " + item.visitor.firstName + "</p>" +
 				"<p>Last name: " + item.visitor.lastName + "</p>" +
-				"<p>Event: " + item.event.id + "</p>"
+				"<p>Attended? " + item.didAttend + "</p>" +
+				"<p>Event ID: " + item.event.id + "</p>" +
+				"<p>Event Name: " + item.event.name + "</p>"
 			); //end of .append
 		}) //end of .each
 	});
