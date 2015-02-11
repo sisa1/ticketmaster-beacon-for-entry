6cc7a15781d9af31c4a7890c52e1c02c330b6865
Shelby <sisa1@asu.edu>
2015-02-11 12:45:30 -0700

    RosterTest: attempt 15

--- BeaconServlet/WebContent/RosterTest.js
+++ BeaconServlet/WebContent/RosterTest.js
@@ -3,22 +3,20 @@ $(document).ready(function(){
 		url: "/BeaconServlet/api/rest/Roster/Event/1"
 	}).then(function(data) {
 		
-		//**************************//
-		//		for each item		//
-		//**************************//
 		$.each(data, function(i, item) {
 			$("#results").append(
-				"<p>Visitor ID: " + item[0].id + "</p>"
-/*				"<p>Visitor last name: " + item[0].lastName + "</p>" +
-				"<p>Visitor first name: " + item[0].firstName + "</p>" +
-				"<p>Visitor Username: " + item[0].username + "</p>" +
-				"<p>Visitor Password: " + item[0].password + "</p>" +
+				"<h2>NEXT VISITOR:</h2>" +
+				"<p>Visitor ID: " + item.visitor.id + "</p>" +
+				"<p>Visitor last name: " + item.visitor.lastName + "</p>" +
+				"<p>Visitor first name: " + item.visitor.firstName + "</p>" +
+				"<p>Visitor Username: " + item.visitor.username + "</p>" +
+				"<p>Visitor Password: " + item.visitor.password + "</p>" +
 				
 				"<p>Attended? " + item.didAttend + "</p>" +
 				
-				"<p>Event ID: " + item[2].id + "</p>" +
-				"<p>Event name: " + item[2].name + "</p>" +
-				"<p></p>"*/
+				"<p>Event ID: " + item.event.id + "</p>" +
+				"<p>Event name: " + item.event.name + "</p>" +
+				"<p></p>"
 			);			
 		}) //end of .each item
 		
