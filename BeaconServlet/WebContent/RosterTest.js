b8def3c9a23cf8145e8776e82138ec860fb82a2a
Shelby <sisa1@asu.edu>
2015-02-11 12:07:00 -0700

    RosterTest: attempt 9

--- BeaconServlet/WebContent/RosterTest.js
+++ BeaconServlet/WebContent/RosterTest.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
 		//**************************//
 		$.each(data, function(i, item) {
 			$("#results").append(
-				"<p>Visitor ID: " + item.visitor[i].id + "</p>" +
-				"<p>Visitor Username: " + item.visitor[i].username + "</p>" +
-				"<p>Visitor Password: " + item.visitor[i].password + "</p>" +
-				"<p>First name: " + item.visitor[i].firstName + "</p>" +
-				"<p>Last name: " + item.visitor[i].lastName + "</p>" +
+				"<p>Visitor ID: " + item.visitor[0].id + "</p>" +
+				"<p>Visitor Username: " + item.visitor[0].username + "</p>" +
+				"<p>Visitor Password: " + item.visitor[0].password + "</p>" +
+				"<p>First name: " + item.visitor[0].firstName + "</p>" +
+				"<p>Last name: " + item.visitor[0].lastName + "</p>" +
 				
 				"<p>Attended? " + item.didAttend + "</p>" +
 				
-				"<p>Event ID: " + item.event[i].id + "</p>" +
-				"<p>Event Name: " + item.event[i].name + "</p>" +
+				"<p>Event ID: " + item.event[0].id + "</p>" +
+				"<p>Event Name: " + item.event[0].name + "</p>" +
 				"<p></p>"
 			);			
 		}) //end of .each item
