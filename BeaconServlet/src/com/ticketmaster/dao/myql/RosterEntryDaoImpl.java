536279ab813576204d308758165788ccd1809b42
mldelaro <mldelaro@asu.edu>
2015-02-17 19:42:35 -0700

    Implement get all entries

--- BeaconServlet/src/com/ticketmaster/dao/myql/RosterEntryDaoImpl.java
+++ BeaconServlet/src/com/ticketmaster/dao/myql/RosterEntryDaoImpl.java
@@ -17,7 +17,43 @@ import com.ticketmaster.dao.RosterEntryDao;
 public class RosterEntryDaoImpl extends MySqlDao implements RosterEntryDao {
 	
 	public List<RosterEntryBean> getAllRosterEntries() {
-		return null;
+		Connection con = null;
+		List<RosterEntryBean> result = new ArrayList<RosterEntryBean>();
+		con = MySqlDao.getConnection();
+		
+		try {
+			String selectAllQuery = "SELECT * FROM eventRoster, users, events WHERE eventRoster.UserId=users.Id AND eventRoster.EventId=events.EventId;";
+			PreparedStatement pStatement = con.prepareStatement(selectAllQuery);
+			ResultSet rs = pStatement.executeQuery();
+			
+			// Iterate ResultSet and Initialize Camera list
+			while(rs.next()) {
+				EventBean eventToAddToEntry = new EventBean();
+				eventToAddToEntry.setId(Integer.parseInt(rs.getString("EventId")));
+				eventToAddToEntry.setName(rs.getString("EventName"));
+				
+				UserBean userToAddToEntry = new UserBean();
+				userToAddToEntry.setId(Integer.parseInt(rs.getString("Id")));
+				userToAddToEntry.setFirstName(rs.getString("FirstName"));
+				userToAddToEntry.setLastName(rs.getString("LastName"));
+				userToAddToEntry.setUsername(rs.getString("Username"));
+				
+				boolean userAttendedEntry = rs.getBoolean("AttendedFlag");
+				
+				RosterEntryBean entry = new RosterEntryBean(eventToAddToEntry, userToAddToEntry, userAttendedEntry);
+				result.add(entry);
+			}
+			
+			pStatement.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			result = null;
+		} finally {
+			// close the connection even if get was unsuccessful
+			MySqlDao.cleanup(con);
+		}
+		return result;
 	}
 	public List<RosterEntryBean> getRosterForEvent(int eventId) {
 		return null;
