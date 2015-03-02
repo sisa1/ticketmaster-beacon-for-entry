eb23bd8459ba84f171c416cc0544d9c11e88afe1
Shelby <sisa1@asu.edu>
2015-03-02 12:10:22 -0700

    Servlet Poll Test: attempt 3

--- BeaconServlet/src/com/ticketmaster/servlet/AsynchPolling.java
+++ BeaconServlet/src/com/ticketmaster/servlet/AsynchPolling.java
@@ -1,12 +1,8 @@
 package com.ticketmaster.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
-import javax.servlet.AsyncContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,55 +14,38 @@ import com.ticketmaster.bean.UserBean;
 import com.ticketmaster.dao.MySqlDaoFactory;
 import com.ticketmaster.dao.UserDao;
 
-@WebServlet(urlPatterns = {"/asynchPolling"}, asyncSupported=true)
+@WebServlet(urlPatterns = {"/asynchPolling"})
 public class AsynchPolling extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-    private List<AsyncContext> contexts = new LinkedList<>();
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String message = request.getParameter("message");
+        //String htmlMessage = "<p><b>" + name + "</b><br/>" + message + "</p>";
     	
-    	PrintWriter printWriter  = response.getWriter();
+    	String htmlMessage = "";
+        ServletContext sc = request.getServletContext();
+        
         try {
 	        UserDao userDao = MySqlDaoFactory.getUserDAO();
 	        List<UserBean> userList = userDao.getAllUsers();
 	        for(int i = 0; i < userList.size(); i++) {
-	        	printWriter.println("<br/>" + userList.get(i).getUsername());
+	        	htmlMessage = "<br/>" + userList.get(i).getUsername();
+	        	
+	        	if (sc.getAttribute("entries") == null) {
+	                sc.setAttribute("entries", htmlMessage);
+	            } else {
+	                String currentMessages = (String) sc.getAttribute("entries");
+	                sc.setAttribute("entries", htmlMessage + currentMessages);
+	            }
+	        	
 	        }
         } catch (Exception Ex) {
-        	printWriter.println("<h2>Error retrieving all users</h2>");
+        	sc.setAttribute("entries", "<h2>Error retrieving all users</h2>");
         }
-    	
-        /*final AsyncContext asyncContext = request.startAsync(request, response);
-        asyncContext.setTimeout(10 * 60 * 1000);
-        contexts.add(asyncContext);*/
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {        
-/*        List<AsyncContext> asyncContexts = new ArrayList<>(this.contexts);
-        this.contexts.clear();
-        String name = request.getParameter("name");
-        String message = request.getParameter("message");
-        String htmlMessage = "<p><b>" + name + "</b><br/>" + message + "</p>";
-        ServletContext sc = request.getServletContext();
-        if (sc.getAttribute("messages") == null) {
-            sc.setAttribute("messages", htmlMessage);
-        } else {
-            String currentMessages = (String) sc.getAttribute("messages");
-            sc.setAttribute("messages", htmlMessage + currentMessages);
-        }
-        for (AsyncContext asyncContext : asyncContexts) {
-            try (PrintWriter writer = asyncContext.getResponse().getWriter()) {
-                writer.println(htmlMessage);
-                writer.flush();
-                asyncContext.complete();
-            } catch (Exception ex) {
-            }
-        }*/
-
+        
+        response.sendRedirect("Polling.jsp");
     }
 }
