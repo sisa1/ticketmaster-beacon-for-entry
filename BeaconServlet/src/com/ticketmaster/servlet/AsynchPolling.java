4ed14a0526999484402df70faea3044aea4908d5
Shelby <sisa1@asu.edu>
2015-03-02 12:33:00 -0700

    Servlet Poll Test: attempt 4

--- BeaconServlet/src/com/ticketmaster/servlet/AsynchPolling.java
+++ BeaconServlet/src/com/ticketmaster/servlet/AsynchPolling.java
@@ -1,6 +1,7 @@
 package com.ticketmaster.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -10,9 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.ticketmaster.bean.UserBean;
+import com.ticketmaster.bean.RosterEntryBean;
 import com.ticketmaster.dao.MySqlDaoFactory;
-import com.ticketmaster.dao.UserDao;
+import com.ticketmaster.dao.RosterEntryDao;
+import com.ticketmaster.dao.EventDao;
 
 @WebServlet(urlPatterns = {"/asynchPolling"})
 public class AsynchPolling extends HttpServlet {
@@ -21,18 +23,18 @@ public class AsynchPolling extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String name = request.getParameter("name");
-        String message = request.getParameter("message");
-        //String htmlMessage = "<p><b>" + name + "</b><br/>" + message + "</p>";
-    	
+    	int eventId = Integer.parseInt(request.getParameter("eventId"));
     	String htmlMessage = "";
         ServletContext sc = request.getServletContext();
         
         try {
-	        UserDao userDao = MySqlDaoFactory.getUserDAO();
-	        List<UserBean> userList = userDao.getAllUsers();
-	        for(int i = 0; i < userList.size(); i++) {
-	        	htmlMessage = "<br/>" + userList.get(i).getUsername();
+        	List<RosterEntryBean> roster = new ArrayList<RosterEntryBean>();
+    		RosterEntryDao rosterDAO = MySqlDaoFactory.getRosterEntryDAO();
+    		roster = rosterDAO.getRosterForEvent(eventId);
+//	        UserDao userDao = MySqlDaoFactory.getUserDAO();
+//	        List<UserBean> userList = userDao.getAllUsers();
+	        for(int i = 0; i < roster.size(); i++) {
+	        	htmlMessage = "<br/>" + roster.get(i).getEvent().getName();
 	        	
 	        	if (sc.getAttribute("entries") == null) {
 	                sc.setAttribute("entries", htmlMessage);
