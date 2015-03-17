1d003b1c00ee784514986a1d9efb7bf5837df087
mldelaro <mldelaro@asu.edu>
2015-03-17 09:34:17 -0700

    quick-fix eventBeaconDao

--- BeaconServlet/src/com/ticketmaster/dao/myql/EventBeaconDaoImpl.java
+++ BeaconServlet/src/com/ticketmaster/dao/myql/EventBeaconDaoImpl.java
@@ -7,13 +7,14 @@ import com.ticketmaster.dao.EventBeaconDao;
 public class EventBeaconDaoImpl implements EventBeaconDao {
 	/** Standard - CRUD operation **/
 	public EventBeaconBean createEventBeacon(EventBeaconBean eventBeacon) {
+		return null;
 		
 	}
 	public EventBeaconBean readEventBeacon(int id) {
-		
+		return null;
 	}
 	public EventBeaconBean updateEventBeacon(int id, EventBeaconBean eventBeacon) {
-		
+		return null;
 	}
 	public void deleteEventBeacon(int id) {
 		
