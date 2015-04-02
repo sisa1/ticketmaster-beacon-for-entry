267ae3a06a185a66c10eddc5c311fd53dc56060e
Shelby <sisa1@asu.edu>
2015-04-01 23:11:30 -0700

    UI update: implemented Event History page. Still needs a start time, time of scan makes no sense

--- BeaconServlet/WebContent/EditSettingsBtn.js
+++
@@ -1,9 +0,0 @@
-//******************************************************//
-//					choose event btn					//
-//******************************************************//
-function chooseEventClick() { 
-	var $event = document.getElementById("choose-event");
-	//$eventNum = $event.options[$event.selectedIndex].value;
-	
-	alert($event.options[$event.selectedIndex].value);
-};
